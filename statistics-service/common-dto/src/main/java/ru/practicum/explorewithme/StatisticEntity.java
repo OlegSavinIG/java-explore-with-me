61485b5a970e059a77ac33a4b159328f3360d1c4
Oleg <oleg211190gm@yandex.ru>
2024-06-28 13:25:19 +0300

    pom and docker finished

---
+++ statistics-service/common-dto/src/main/java/ru/practicum/explorewithme/StatisticEntity.java
@@ -0,0 +1,26 @@
+package ru.practicum.explorewithme;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "statistic")
+public class StatisticEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String app;
+    private String uri;
+    private String ip;
+    @Column(name = "creation_time")
+    private LocalDateTime creationTime;
+}
---
+++ statistics-service/common-dto/src/main/java/ru/practicum/explorewithme/StatisticRequest.java
@@ -0,0 +1,28 @@
+package ru.practicum.explorewithme;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatisticRequest {
+    @NotNull
+    @NotBlank
+    private String app;
+    @NotNull
+    @NotBlank
+    private String uri;
+    @NotNull
+    @NotBlank
+    private String ip;
+    @NotNull
+    private LocalDateTime creationTime;
+}
