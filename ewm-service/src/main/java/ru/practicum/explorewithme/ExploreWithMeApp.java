29b01daea598b99d36bcfeba03e8fedc4c3da726
Oleg <oleg211190@gmail.com>
2024-07-04 16:55:09 +0300

    checkstyle issues

--- ewm-service/src/main/java/ru/practicum/explorewithme/ExploreWithMeApp.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/ExploreWithMeApp.java
@@ -3,9 +3,23 @@ package ru.practicum.explorewithme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application class for ExploreWithMe.
+ */
 @SpringBootApplication
 public class ExploreWithMeApp {
-    public static void main(String[] args) {
+
+    // Private constructor to prevent instantiation
+    private ExploreWithMeApp() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
+    /**
+     * The main method which serves as the entry point for the application.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(ExploreWithMeApp.class, args);
     }
 }
